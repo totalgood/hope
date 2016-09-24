84715eaa9632fa8165fa1481890480e7d921b51a
Hobson Lane <github@totalgood.com>
2016-09-24 12:01:59 -0700

    move revealjs slides to docs/slides/*

---
+++ hope/chatterbot_mongo_terminal.py
@@ -0,0 +1,30 @@
+from chatterbot import ChatBot
+import logging
+
+
+# Uncomment the following line to enable verbose logging
+# logging.basicConfig(level=logging.INFO)
+
+# Create a new ChatBot instance
+bot = ChatBot('Terminal',
+    storage_adapter='chatterbot.adapters.storage.MongoDatabaseAdapter',
+    logic_adapters=[
+        'chatterbot.adapters.logic.ClosestMatchAdapter'
+    ],
+    filters=[
+        'chatterbot.filters.RepetitiveResponseFilter'
+    ],
+    input_adapter='chatterbot.adapters.input.TerminalAdapter',
+    output_adapter='chatterbot.adapters.output.TerminalAdapter',
+    database='chatterbot-database'
+)
+
+print('Type something to begin...')
+
+while True:
+    try:
+        bot_input = bot.get_response(None)
+
+    # Press ctrl-c or ctrl-d on the keyboard to exit
+    except (KeyboardInterrupt, EOFError, SystemExit):
+        break
